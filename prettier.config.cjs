3ecb30bbf90b617f0ede646644cd091af4ee32bb
Adrià Navarrete <adri.navarrete.09@gmail.com>
2023-03-25 19:05:46 +0100

    Add airbnb lint

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 110,
+  singleQuote: true,
+  useTabs: true,
+  semi: false,
 };
 
 module.exports = config;
